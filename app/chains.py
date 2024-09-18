24be0c872b0a1547a97125eb4908c1ef34f1424c
dhanvanth342 <voonadhanvanth183@gmail.com>
2024-09-17 20:50:24 -0400

    First commit
    This is the outline of the project, the vector database and web interface will be updated in the future commits

---
+++ app/chains.py
@@ -0,0 +1,61 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.1-70b-versatile")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+                """
+            ### JOB DESCRIPTION:
+            {job_description}
+            
+            ### INSTRUCTION:
+            
+            You are Dhanvanth voona currently pursuing master of Data science at Illinois institute of technology
+            chicago. You have experience with different skills available in the vector database and mention
+            only the ones which are matching to the job description. 
+            Now,
+            your job is to write a cold email to the company of job posted writing your interest in working
+            as intern and open to give more details about you. Keep the mail short, point wise,
+            and cool with out of box reply.
+            Also add the most relevant ones from the following
+            links to showcase your portfolio: {link_list}
+            ### EMAIL (NO PREAMBLE):
+            
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
